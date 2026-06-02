fd5c41bf991e7c0fb2c4ca00c091741f1094b5f1
tiago <tfmoraes@gmail.com>
2026-06-02 20:50:47 -0300

    projeto inicial do sistema de alarmes

---
+++ src/main/java/com/tiago/alarmes/AlarmesApplication.java
@@ -0,0 +1,13 @@
+package com.tiago.alarmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlarmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlarmesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiago/alarmes/AlarmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.alarmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlarmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
