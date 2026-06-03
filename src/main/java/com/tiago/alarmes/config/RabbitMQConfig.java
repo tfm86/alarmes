2b32b0b6354c841e93a8232d0d3ce44a276c8d5c
tiago <tfmoraes@gmail.com>
2026-06-03 00:58:49 -0300

    OSS Simples final

--- src/main/java/com/tiago/alarmes/config/RabbitMQConfig.java
+++ src/main/java/com/tiago/alarmes/config/RabbitMQConfig.java
@@ -1,8 +1,13 @@
 package com.tiago.alarmes.config;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RabbitMQConfig {
 
     public static final String FILA_ALARMES = "alarmes.fila";
@@ -12,5 +17,16 @@ public class RabbitMQConfig {
         return new Queue(FILA_ALARMES, true);
     }
 
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         Jackson2JsonMessageConverter messageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter);
+        return template;
+    }
 }
--- src/main/java/com/tiago/alarmes/consumer/AlarmeConsumer.java
+++ src/main/java/com/tiago/alarmes/consumer/AlarmeConsumer.java
@@ -2,6 +2,8 @@ package com.tiago.alarmes.consumer;
 
 import com.tiago.alarmes.model.Alarme;
 import com.tiago.alarmes.service.AlarmeService;
+import com.tiago.alarmes.config.RabbitMQConfig;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,9 +15,9 @@ public class AlarmeConsumer {
         this.service = service;
     }
 
+    @RabbitListener(queues = RabbitMQConfig.FILA_ALARMES)
     public void receberMensagem(Alarme alarme) throws IllegalAccessException {
+        System.out.println("Alarme recebido da fila: " + alarme);
         service.salvar(alarme);
-        System.out.println("Alarme recebido da fila:" + alarme);
     }
-
 }
